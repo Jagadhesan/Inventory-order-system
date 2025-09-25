be131b6a51fa6d6aa581f6cbe9215086c7e97890
Jagadhesan <jagadhesan_j@srmap.edu.in>
2025-09-25 18:25:34 +0530

    Added .gitignore for Maven project

---
+++ src/test/java/com/amazon_inventory/Inventory_order_system/InventoryOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon_inventory.Inventory_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
